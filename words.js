70cddeb67c7da1997154ceb9169a1119ef369fef
MrJoCrafter <cstudios35511@gmail.com>
2020-04-13 17:55:03 -0500

    Fix Words

--- words.js
+++ words.js
@@ -2,9 +2,9 @@
 var franchisea = "Steven Universe,Adventure Time,Ninjago: Masters of Spinjitsu,";
 var franchiseb = "Once Upon A Time";
 //Split Arrays
-var farraya = franschises.split(",");
-var farrayb = franschises.split(",");
+var farraya = franchisea.split(",");
+var farrayb = franchiseb.split(",");
 
 
 //Functions
-var lines = [ farraya, [" characters in a  "], farrayb, ["setting."]];
+var lines = [ farraya, [" characters in a  "], farrayb, ["setting."],];
