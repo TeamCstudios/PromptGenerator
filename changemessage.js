d527875f2dae6764c315ebc37cd58e90cb2f7663
MrJoCrafter <cstudios35511@gmail.com>
2020-04-14 11:06:58 -0500

    fix error

--- changemessage.js
+++ changemessage.js
@@ -5,7 +5,6 @@ function changeMessage(){
     document.getElementById("1line").innerHTML=lines[1][(Math.floor(Math.random()*lines[1].length))];
     document.getElementById("2line").innerHTML=lines[2][randb];
     document.getElementById("3line").innerHTML=lines[3][(Math.floor(Math.random()*lines[3].length))];
-    document.getElementById("4line").innerHTML=lines[4][(Math.floor(Math.random()*lines[4].length))];
     document.getElementById("0lineb").innerHTML=prompts[randb][(Math.floor(Math.random()*prompts[randb].length))];
     document.getElementById("startBttn").textContent = "Again?";
 }
