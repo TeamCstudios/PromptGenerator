ad08b19f8154caa9254da2dd7a96890d00baecf1
MrJoCrafter <cstudios35511@gmail.com>
2020-04-14 09:56:01 -0500

    Working on the actual Prompt Part

--- changemessage.js
+++ changemessage.js
@@ -1,6 +1,11 @@
 function changeMessage(){
-    for(var i=0; i<5; i++){
-        document.getElementById(i+"line").innerHTML=lines[i][(Math.floor(Math.random()*lines[i].length))];
-    }
+    var randa = (Math.floor(Math.random()*lines[0].length));
+    var randb = (Math.floor(Math.random()*lines[2].length));
+    document.getElementById("0line").innerHTML=lines[0][randa];
+    document.getElementById("1line").innerHTML=lines[1][(Math.floor(Math.random()*lines[1].length))];
+    document.getElementById("2line").innerHTML=lines[2][randb];
+    document.getElementById("3line").innerHTML=lines[3][(Math.floor(Math.random()*lines[3].length))];
+    document.getElementById("4line").innerHTML=lines[4][(Math.floor(Math.random()*lines[4].length))];
+    document.getElementById("0lineb").textContent=prompts[randa][(Math.floor(Math.random()*prompts[randa].length))];
     document.getElementById("startBttn").textContent = "Again?";
 }
--- words.js
+++ words.js
@@ -1,10 +1,12 @@
 //Word Lists
-var franchisea = "Steven Universe,Adventure Time,Ninjago: Masters of Spinjitsu,";
+var franchisea = "Steven Universe,Adventure Time,Ninjago: Masters of Spinjitsu,Once Upon a Time,She-Ra (2018),Pokémon (Games),Pokémon (Anime)";
 var franchiseb = "Once Upon A Time";
+var ouatprompts = "Characters are cursed and sent to a mundane town to with a new identity and new memories.,One character is now the Dark One and must struggle against their own dark magic.,Characters are cursed and sent to a mundane town to with a new identity and new memories.,Characters are cursed and sent to a mundane town to with a new identity and new memories.";
 //Split Arrays
 var farraya = franchisea.split(",");
 var farrayb = franchiseb.split(",");
-
+var ouatarray = ouatprompts.split(",");
 
 //Functions
 var lines = [ farraya, [" characters in a  "], farrayb, ["setting."],];
+var prompts = [ ouatprompts, ouatprompts];
