2ee8049346cbc6d7f263526a2870b557eb3df255
MrJoCrafter <cstudios35511@gmail.com>
2020-04-14 11:44:36 -0500

    Undertale

--- words.js
+++ words.js
@@ -1,17 +1,19 @@
 //Franchise Lists
-var franchisea = "Steven Universe,Adventure Time,Ninjago: Masters of Spinjitsu,Once Upon a Time,She-Ra (2018),Pokémon (Games),Pokémon (Anime)";
-var franchiseb = "Once Upon A Time,Pokémon,Doctor Who";
+var franchisea = "Steven Universe,Adventure Time,Ninjago: Masters of Spinjitsu,Once Upon a Time,She-Ra (2018),Pokémon (Games),Pokémon (Anime),Undertale";
+var franchiseb = "Once Upon A Time,Pokémon,Doctor Who,Undertale"";
 //Prompt Lists
 var ouatprompts = "Characters are cursed and sent to a mundane town to with a new identity and new memories.,One character is now the Dark One and must struggle against their own dark magic.,Characters are cursed and sent to a mundane town to with a new identity and new memories.,Characters are cursed and sent to a mundane town to with a new identity and new memories.";
 var pkmnprompts = "Several characters are Pokémon Trainers in the Kalos Region.,Several characters are Pokémon Trainers in the Kanto Region.,Several characters are Pokémon Trainers in the Sinnoh Region.,Several characters are Pokémon Trainers in the Johto Region.,Several characters are Pokémon Trainers in the Hoenn Region."
 var whoprompts = "One character is a Time Lord and has a TARDIS."
+var undrprompts = "Characters were banished to The Underground long ago. Another character, original or not, falls into the Underground and meets all the characters trapped inside."
 //Split Arrays
 var farraya = franchisea.split(",");
 var farrayb = franchiseb.split(",");
 var ouatarray = ouatprompts.split(",");
 var pkmnarray = pkmnprompts.split(",");
 var whoarray = whoprompts.split(",");
+var undrarray = undrprompts.split(",");
 
 //Functions
 var lines = [ farraya, [" characters in a  "], farrayb, ["setting."],];
-var prompts = [ ouatarray, pkmnarray, whoarray];
+var prompts = [ ouatarray, pkmnarray, whoarray, undrarray];
