2c67b8815d7833166bdd2330762b9f586a9852dc
MrJoCrafter <cstudios35511@gmail.com>
2020-04-14 11:04:15 -0500

    Add Pokemon Prompts

--- words.js
+++ words.js
@@ -1,12 +1,14 @@
 //Word Lists
 var franchisea = "Steven Universe,Adventure Time,Ninjago: Masters of Spinjitsu,Once Upon a Time,She-Ra (2018),Pokémon (Games),Pokémon (Anime)";
-var franchiseb = "Once Upon A Time";
+var franchiseb = "Once Upon A Time,Pokémon";
 var ouatprompts = "Characters are cursed and sent to a mundane town to with a new identity and new memories.,One character is now the Dark One and must struggle against their own dark magic.,Characters are cursed and sent to a mundane town to with a new identity and new memories.,Characters are cursed and sent to a mundane town to with a new identity and new memories.";
+var pkmnprompts = "Several characters are Pokémon Trainers in the Kalos Region.,Several characters are Pokémon Trainers in the Kanto Region.,Several characters are Pokémon Trainers in the Sinnoh Region.,Several characters are Pokémon Trainers in the Johto Region.,Several characters are Pokémon Trainers in the Hoenn Region."
 //Split Arrays
 var farraya = franchisea.split(",");
 var farrayb = franchiseb.split(",");
 var ouatarray = ouatprompts.split(",");
+var pkmnarray = pkmnprompts.split(",");
 
 //Functions
 var lines = [ farraya, [" characters in a  "], farrayb, ["setting."],];
-var prompts = [ ouatarray, [" testa "],];
+var prompts = [ ouatarray, pkmnarray,];
