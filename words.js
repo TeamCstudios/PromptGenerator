3bb49bdff785c3a9f9f9e22936740db48d94567f
MrJoCrafter <cstudios35511@gmail.com>
2020-04-14 11:54:29 -0500

    fix the undertale prompt, i'm an idiot.

--- words.js
+++ words.js
@@ -5,7 +5,7 @@ var franchiseb = "Once Upon A Time,Pokémon,Doctor Who,Undertale,Death Note";
 var ouatprompts = "Characters are cursed and sent to a mundane town to with a new identity and new memories.,One character is now the Dark One and must struggle against their own dark magic.,Characters are cursed and sent to a mundane town to with a new identity and new memories.,Characters are cursed and sent to a mundane town to with a new identity and new memories.";
 var pkmnprompts = "Several characters are Pokémon Trainers in the Kalos Region.,Several characters are Pokémon Trainers in the Kanto Region.,Several characters are Pokémon Trainers in the Sinnoh Region.,Several characters are Pokémon Trainers in the Johto Region.,Several characters are Pokémon Trainers in the Hoenn Region."
 var whoprompts = "One character is a Time Lord and has a TARDIS."
-var undrprompts = "Characters were banished to The Underground long ago. Another character, original or not, falls into the Underground and meets all the characters trapped inside."
+var undrprompts = "Characters were banished to The Underground long ago. Another character (original or not) falls into the Underground and meets all the characters trapped inside."
 var denoprompts = "One character obtains a Death Note and uses it to exact vengance on who they see as evil. Another character or characters make it their mission to stop them.,Multiple characters obtain a Death Note and use it to each exact their own ideologies. The world falls into chaos."
 //Split Arrays
 var farraya = franchisea.split(",");
